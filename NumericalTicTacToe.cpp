5a7d41d5746a04c519e89ce63dfbb2a8e98e924b
Asmaa Farouq <asmaafarouq649@gmail.com>
2025-11-22 18:53:51 +0000

    NumericalTicTacToe and menu

---
+++ NumericalTicTacToe.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <limits>
+#include <cstdlib>
+#include <ctime>
+#include "NumericalTicTacToe.h"
+
+using namespace std;
+
+void clearInputBuffer() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+int getValidChoice() {
+    int choice;
+    while (true) {
+        if (cin >> choice) {
+            clearInputBuffer();
+            if (choice >= 0 && choice <= 1) {
+                return choice;
+            }
+        }
+        else {
+            clearInputBuffer();
+        }
+        cout << "Invalid choice. Please enter 0 or 1: ";
+    }
+}
+
+void displayMenu() {
+    cout << "\n>_< Tic Tac Toe Menu >_<\n";
+    cout << "1. Numerical Tic-Tac-Toe\n";
+    cout << "0. Exit\n";
+    cout << "=======================\n";
+    cout << "Enter your choice (0-1): ";
+}
+
+void runNumericalGame() {
+    cout << "\n>_< Starting Numerical Tic-Tac-Toe >_<\n";
+    try {
+        int modeSelection;
+        cout << "Choose game mode:" << endl;
+        cout << "1. Play against another player" << endl;
+        cout << "2. Play against the computer" << endl;
+
+        while (!(cin >> modeSelection) || (modeSelection != 1 && modeSelection != 2)) {
+            clearInputBuffer();
+            cout << "Invalid choice. Please enter 1 or 2: ";
+        }
+        clearInputBuffer();
+
+        NumericalTicTacToe gameBoard;
+        NumericalTicTacToePlayer* playerOne;
+        NumericalTicTacToePlayer* playerTwo;
+
+        string player1Name, player2Name;
+        cout << "Enter name for Player 1: ";
+        getline(cin, player1Name);
+
+        if (modeSelection == 1) {
+            cout << "Enter name for Player 2: ";
+            getline(cin, player2Name);
+            playerOne = new NumericalTicTacToePlayer(player1Name, 1);
+            playerTwo = new NumericalTicTacToePlayer(player2Name, 2);
+        }
+        else {
+            playerOne = new NumericalTicTacToePlayer(player1Name, 1);
+            playerTwo = new NumericalTicTacToePlayer("Computer", 2);
+        }
+
+        NumericalTicTacToeManager gameManager(&gameBoard, playerOne, playerTwo);
+        gameManager.run();
+
+        delete playerOne;
+        delete playerTwo;
+    }
+    catch (...) {
+        cout << "An error occurred during the game.\n";
+    }
+}
+
+int main() {
+    cout << "Welcome to Numerical Tic Tac Toe!\n";
+
+    int choice;
+    do {
+        displayMenu();
+        choice = getValidChoice();
+
+        if (choice == 1) {
+            runNumericalGame();
+            cout << "\nPress Enter to return to the menu...";
+            clearInputBuffer();
+            cin.get();
+        }
+    } while (choice != 0);
+
+    cout << "\nThanks for playing Numerical Tic Tac Toe!\n";
+    return 0;
+}
---
+++ NumericalTicTacToe.h
@@ -0,0 +1,146 @@
+
+#ifndef _NUMERICAL_TIC_TAC_TOE_H
+#define _NUMERICAL_TIC_TAC_TOE_H
+
+#include "BoardGame_Classes.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class NumericalTicTacToe : public Board<int> {
+private:
+    static const int GRID_SIZE = 3;
+
+public:
+    NumericalTicTacToe() {
+        rows = GRID_SIZE;
+        columns = GRID_SIZE;
+        board = new int*[GRID_SIZE];
+        for (int i = 0; i < GRID_SIZE; i++) {
+            board[i] = new int[GRID_SIZE]{0};
+        }
+        n_moves = 0;
+    }
+
+    ~NumericalTicTacToe() {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            delete[] board[i];
+        }
+        delete[] board;
+    }
+
+    bool update_board(int row, int col, int value) override {
+        if (row < 0 || row >= GRID_SIZE || col < 0 || col >= GRID_SIZE || board[row][col] != 0) {
+            return false;
+        }
+        board[row][col] = value;
+        n_moves++;
+        return true;
+    }
+
+    void display_board() override {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    bool is_win() override {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            int rowSum = 0, colSum = 0;
+            for (int j = 0; j < GRID_SIZE; j++) {
+                rowSum += board[i][j];
+                colSum += board[j][i];
+            }
+            if (rowSum == 15 || colSum == 15) {
+                return true;
+            }
+        }
+
+        int mainDiagonalSum = 0, antiDiagonalSum = 0;
+        for (int i = 0; i < GRID_SIZE; i++) {
+            mainDiagonalSum += board[i][i];
+            antiDiagonalSum += board[i][GRID_SIZE - i - 1];
+        }
+
+        return mainDiagonalSum == 15 || antiDiagonalSum == 15;
+    }
+
+    bool is_draw() override {
+        return n_moves == GRID_SIZE * GRID_SIZE && !is_win();
+    }
+
+    bool game_is_over() override {
+        return is_win() || is_draw();
+    }
+};
+
+class NumericalTicTacToePlayer : public Player<int> {
+public:
+    NumericalTicTacToePlayer(string playerName, int playerSymbol) : Player<int>(playerName, playerSymbol) {}
+
+    void getmove(int& row, int& col) override {
+        int number;
+        cout << getname() << ": Enter your move (row column number): ";
+        cin >> row >> col >> number;
+
+        while (!is_valid_move(number) || !boardPtr->update_board(row, col, number)) {
+            cout << "Invalid move. Try again: ";
+            cin >> row >> col >> number;
+        }
+    }
+
+private:
+    bool is_valid_move(int number) {
+        if (symbol % 2 == 1 && number % 2 == 0) {
+            cout << "You must enter an odd number!\n";
+            return false;
+        }
+        if (symbol % 2 == 0 && number % 2 != 0) {
+            cout << "You must enter an even number!\n";
+            return false;
+        }
+        return true;
+    }
+};
+
+class NumericalTicTacToeManager {
+private:
+    NumericalTicTacToe* gameBoardPtr;
+    NumericalTicTacToePlayer* players[2];
+
+public:
+    NumericalTicTacToeManager(NumericalTicTacToe* board, NumericalTicTacToePlayer* player1, NumericalTicTacToePlayer* player2) {
+        gameBoardPtr = board;
+        players[0] = player1;
+        players[1] = player2;
+        players[0]->setBoard(board);
+        players[1]->setBoard(board);
+    }
+
+    void run() {
+        int row, col;
+        gameBoardPtr->display_board();
+
+        while (!gameBoardPtr->game_is_over()) {
+            for (int i = 0; i < 2; i++) {
+                players[i]->getmove(row, col);
+                gameBoardPtr->display_board();
+                if (gameBoardPtr->is_win()) {
+                    cout << players[i]->getname() << " wins!\n";
+                    return;
+                }
+                if (gameBoardPtr->is_draw()) {
+                    cout << "It's a draw!\n";
+                    return;
+                }
+            }
+        }
+    }
+};
+
+#endif // _NUMERICAL_TIC_TAC_TOE_H
