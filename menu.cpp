01c87d7f1345630c0bfa34436769e0f6cbbed03d
Arwa Bashier <arwabashier820@gmail.com>
2025-11-23 18:39:40 +0000

    menuu

--- menu.cpp
+++ menu.cpp
@@ -8,35 +8,95 @@ using namespace std;
 
 void displayMenu() {
     cout << "\n>_< Tic Tac Toe Menu >_<\n";
-    cout << "1. Classic Tic-Tac-Toe\n";
-    cout << "2. Infinity Tic-Tac-Toe\n";
+    cout << "1. SUS game\n";
+    cout << "2. Four-in-a-Row\n";
+    cout << "3. 5x5 Tic-Tac-Toe\n";
+    cout << "4. Word Tic-Tac-Toe\n";
+    cout << "5. Misère Tic-Tac-Toe\n";
+    cout << "6. Diamond Tic-Tac-Toe\n";
+    cout << "7. 4x4 Tic-Tac-Toe\n";
+    cout << "8. Pyramid Tic-Tac-Toe\n";
+    cout << "9. Numerical Tic-Tac-Toe\n";
+    cout << "10. Obstacles Tic-Tac-Toe\n";
+    cout << "11. Infinity Tic-Tac-Toe\n";
+    cout << "12. Ultimate Tic Tac Toe\n";
+    cout << "13. Memory Tic-Tac-Toe\n";
     cout << "0. Exit\n";
-    cout << "Enter your choice: ";
-}
+ }
 
-void runPolymorphicGame(int choice) {
-    // POLYMORPHISM: Base class pointers holding derived objects
+void runGame(int choice) {
+    //base class pointers
     UI<char>* game_ui = nullptr;
     Board<char>* game_board = nullptr;
     
     switch (choice) {
-        case 1:
-            game_ui = new XO_UI();
-            game_board = new X_O_Board();
+        case 1: {
+            cout << "\n>_< Starting SUS Tic-Tac-Toe >_<\n";
+            
             break;
-        case 2:
-            game_ui = new InfinityTicTacToe_UI();
+        }
+        case 2: {
+            cout << "\n>_< Starting Four-in-a-Row Tic-Tac-Toe >_<\n";
+          
+            break;
+        } 
+        case 3: {
+            cout << "\n>_< Starting 5x5 Tic-Tac-Toe >_<\n";
+            
+            break;
+        }
+        case 4: {
+            cout << "\n>_< Starting Word Tic-Tac-Toe >_<\n";
+            
+            break;
+        }
+        case 5:{
+            cout << "\n>_< Starting Misère Tic-Tac-Toe >_<\n";             
+            break;  
+        }
+        case 6: {
+            cout << "\n>_< Starting Diamond Tic-Tac-Toe >_<\n";             
+            break;
+        }
+        case 7: {
+            cout << "\n>_< Starting 4x4 Tic-Tac-Toe >_<\n";             
+            break;
+        }   
+        case 8: {
+            cout << "\n>_< Starting Pyramid Tic-Tac-Toe >_<\n";             
+            break;
+        }     
+        case 9: {
+            cout << "\n>_< Starting Numerical Tic-Tac-Toe >_<\n";             
+            break;
+        }
+        case 10: {
+            cout << "\n>_< Starting Obstacles Tic-Tac-Toe >_<\n";             
+            break;                                          
+        }   
+        case 11:{
+            cout << "\n>_< Starting Infinity Tic-Tac-Toe >_<\n";   
+            game_ui = new InfinityTicTacToe_UI();  //base pointer to derived
             game_board = new InfinityBoard();
-            break;
-        default:
-            return;
+            break;          
+        }
+        case 12: {
+            cout << "\n>_< Starting Ultimate Tic Tac Toe >_<\n";             
+            break;      
+        }   
+        case 13: {      
+            cout << "\n>_< Starting Memory Tic-Tac-Toe >_<\n";             
+            break;      
     }
-    
-    // POLYMORPHIC SETUP: Same code works for all games
+    default:
+        cout << "Invalid choice!\n";
+        return;
+}
+    // ~ ~ polymorphism ~ ~ 
     Player<char>** players = game_ui->setup_players();
     GameManager<char> game_manager(game_board, players, game_ui);
     
-    // POLYMORPHIC EXECUTION: GameManager calls virtual methods
+    // GameManager calls virtual methods
     game_manager.run();
     
     // Cleanup
@@ -52,7 +112,7 @@ int main() {
         displayMenu();
         cin >> choice;
         if (choice != 0) {
-            runPolymorphicGame(choice);
+            runGame(choice);
         }
     } while (choice != 0);
     return 0;
