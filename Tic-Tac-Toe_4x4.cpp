4d7e4d42bf8374596629c19cf8db91562f40b88b
Asmaa Farouq <asmaafarouq649@gmail.com>
2025-11-27 19:31:15 +0000

    added rules

--- Tic-Tac-Toe_4x4.cpp
+++ Tic-Tac-Toe_4x4.cpp
@@ -7,17 +7,16 @@
 using namespace std;
 
 X_O_4x4Board::X_O_4x4Board() : Board(4, 4) {
-    // Initialize all cells with blank_symbol
     for (auto& row : board)
         for (auto& cell : row)
             cell = blank_symbol;
 
-    board[0][1] = 'X'; //First row 
+    board[0][1] = 'X'; //First row in the game
     board[0][3] = 'X';
     board[0][0] = 'O';
     board[0][2] = 'O';
 
-    board[3][0] = 'X'; // Last row
+    board[3][0] = 'X'; // Last row in the came
     board[3][2] = 'X';
     board[3][1] = 'O';
     board[3][3] = 'O';
@@ -38,7 +37,7 @@ bool X_O_4x4Board::update_board(Move<char>* move) {
         return false;
     }
 
-    // Search for an adjacent piece belonging to the player
+    // Search for an adjacent piece belonging to the player to move based on adjacency rule
     int dirs[4][2] = {
         {1,0},  // down
         {-1,0}, // up
@@ -118,7 +117,14 @@ bool X_O_4x4Board::game_is_over(Player<char>* player) {
 
 
 
-X_O_4x4_UI::X_O_4x4_UI() : UI<char>("Weclome to FCAI X-O 4x4 Game", 3) {}
+X_O_4x4_UI::X_O_4x4_UI() : UI<char>("Weclome to FCAI X-O 4x4 Game", 3) {
+    cout << "X-O 4x4 UI initialized.\n";
+    cout << "Players will take turns moving their pieces on a 4x4 board.\n";
+    cout << "To make a move, enter the coordinates of the cell where you want to place your piece.\n";
+    cout << "You can only move to an adjacent cell next to one of your existing pieces.\n";
+    cout << "The goal is to align three of your pieces in a row, column, or diagonal.\n";
+    cout << "Ready?, Enjoy the game!\n";
+}
 
 Player<char>* X_O_4x4_UI::create_player(string& name, char symbol, PlayerType type) {
     // Create player based on type
